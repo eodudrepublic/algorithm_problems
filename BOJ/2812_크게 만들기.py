be30d7de5b23facdda5db837a2309e1023e1802a
Daeyoung Kim <eodudrepublic@gmail.com>
2023-10-25 00:11:18 +0900

    2023-10-23/24 solve

--- BOJ/2812_크게 만들기.py
+++ BOJ/2812_크게 만들기.py
@@ -1,24 +1,11 @@
 import sys
+from collections import deque
 
 n, k = map(int, sys.stdin.readline().split())
-num = list(sys.stdin.readline().rstrip())
+num = deque(sys.stdin.readline().rstrip())
+returnNum = deque()
 
-t = 0
-part = num[0:k+1]
-while k != 0 :
-    # print(part)
-    if part[0] != max(part) :
-        i = part.index(max(part))
-        del part[0:i]
-        del num[t:t+i]
-        k -= i
-    else : 
-        t += 1
-        if n == t+k : 
-            del num[t:]
-            break
-        part.append(num[t+k])
-        del part[0]
-
-for _ in num : sys.stdout.write(_)
-sys.stdout.write('\n')
+while k != 0 and n != k :
+    a = num.popleft()
+    b = num.popleft()
+    # 모르겠다...
