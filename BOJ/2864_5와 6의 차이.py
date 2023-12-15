9cd4f9146eee1e0c73ef703d9bb1f99208f1ac95
Daeyoung Kim <eodudrepublic@gmail.com>
2023-12-16 01:12:23 +0900

    2023-12-15 solve

---
+++ BOJ/2864_5와 6의 차이.py
@@ -0,0 +1,7 @@
+# 2023-12-15 (12-16)
+a, b = input().split()
+min_a = a.replace('6', '5')
+max_a = a.replace('5', '6')
+min_b = b.replace('6', '5')
+max_b = b.replace('5', '6')
+print(int(min_a) + int(min_b), int(max_a) + int(max_b))
