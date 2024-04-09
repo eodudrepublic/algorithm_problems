f4ca2611e3762b273a0886bc54701c3413e1b1ad
Daeyoung Kim <eodudrepublic@gmail.com>
2024-04-09 11:34:00 +0900

    2024-04-09 solve

--- BOJ/2812_크게 만들기.py
+++ BOJ/2812_크게 만들기.py
@@ -1,11 +1,35 @@
+# 2024-04-09
 import sys
 from collections import deque
 
 n, k = map(int, sys.stdin.readline().split())
+_index = n - k
 num = deque(sys.stdin.readline().rstrip())
-returnNum = deque()
+ans = deque([num.popleft()])
+while num :
+    temp1 = num.popleft()
+    temp2 = ans.pop()
+    if temp1 < temp2 :
+        ans.append(temp2)
+        ans.append(temp1)
+    elif temp1 == temp2 :
+        ans.append(temp2)
+        ans.append(temp1)
+    else :
+        if ans :
+            num.appendleft(temp1)
+        else :
+            ans.append(temp1)
+        k -= 1
+        if k == 0 : break
 
-while k != 0 and n != k :
-    a = num.popleft()
-    b = num.popleft()
-    # 모르겠다...
+_ = 0
+while ans :
+    sys.stdout.write(str(ans.popleft()))
+    _ += 1
+    if _ == _index : break
+while num :
+    if _ == _index : break
+    sys.stdout.write(str(num.popleft()))
+    _ += 1
+sys.stdout.write('\n')
