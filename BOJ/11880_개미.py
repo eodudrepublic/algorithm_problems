3f3acf7706ab51c34da6c9f60cb2ed11aa7af586
Daeyoung Kim <eodudrepublic@gmail.com>
2024-04-11 01:22:32 +0900

    2024-04-10 solve

---
+++ BOJ/11880_개미.py
@@ -0,0 +1,8 @@
+# 2024-04-10 (04-11)
+import sys
+
+T = int(sys.stdin.readline())
+for t in range(T) :
+    a, b, c = map(int, sys.stdin.readline().split())
+    temp = max(a, b, c)
+    sys.stdout.write(''.join([str((a + b + c - temp)**2 + temp**2), '\n']))
