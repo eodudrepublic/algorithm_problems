8ad411437e1447bab572d417e3072f8a942e5270
Daeyoung Kim <eodudrepublic@gmail.com>
2024-04-12 22:56:15 +0900

    2024-04-12 solve 와 PyPy3로 해야할 줄은 몰랐네... 자존심 상해;;;

--- BOJ/3063_게시판.py
+++ BOJ/3063_게시판.py
@@ -1,15 +1,17 @@
+# 2024-04-12
 import sys
-
-def gets(p1, p2) :
-    s = (p1[0] - p2[0]) * (p1[1] - p2[1])
-    if s < 0 : return -1 * s
-    else : return s
-    
     
 T = int(sys.stdin.readline())
 for t in range(T) :
     x1, y1, x2, y2, x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
     
     size = 0
+    h = y2 - y1
     for x in range(x1, x2) :
-        pass
+        if x3 <= x and x < x4 :
+            for y in range(y1, y2) :
+                if y3 > y or y >= y4 :
+                    size += 1
+        else :
+            size += h
+    sys.stdout.write(''.join([str(size), '\n']))
